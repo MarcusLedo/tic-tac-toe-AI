1b1a20c2c6379fc1c64255007cbc76a900f7bbd2
Marcus Ledo <sabaledo27@hotmail.com>
2023-08-04 19:11:38 -0300

    Initial commit

---
+++ tictactoe.py
@@ -0,0 +1,16 @@
+import sys  # auxiliar na saída do jogo
+import pygame
+
+from constants import *
+
+# PYGAME
+pygame.init()
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption('TIC TAC TOE')
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
